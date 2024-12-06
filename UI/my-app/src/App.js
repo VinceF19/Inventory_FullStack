9135f62101fb1af9b4fd80f5648f7a9d541cbf77
Vince Fernandez <Vince.fernandezg@gmail.com>
2024-12-06 15:29:23 +0800

    ui

--- UI/my-app/src/App.js
+++ UI/my-app/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { Home } from './Home';
+import { Category } from './Category';
+import { Item } from './Item';
+import { BrowserRouter as Router, Route, Routes, NavLink } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App Container">
+        <h3 className="d-flex justify-content-center m-3">React JS Frontend</h3>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav">
+            <li className="nav-item m-1">
+              <NavLink to="/home" className="btn btn-light btn-outline-primary">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item m-1">
+              <NavLink to="/category" className="btn btn-light btn-outline-primary">
+                Category
+              </NavLink>
+            </li>
+            <li className="nav-item m-1">
+              <NavLink to="/item" className="btn btn-light btn-outline-primary">
+                Item
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/item" element={<Item />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ UI/my-app/src/Item.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Item = () => {
+  return (
+    <div>
+      <h3>This is Item page</h3>
+    </div>
+  );
+};
